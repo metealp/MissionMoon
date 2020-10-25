6f9e212928f15f0bc0ba5641ec094e860f1202ab
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-25 19:01:04 +0300

    Initial Commit

---
+++ MissionMoon/MissionMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionMoonApp.swift
+//  MissionMoon
+//
+//  Created by Mete Alp Kizilcay on 25.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct MissionMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
