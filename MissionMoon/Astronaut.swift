1ab0b7fe645ffd3c99347e08c72f93ae1e678de6
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-25 22:44:45 +0300

    add views, json parse and models

---
+++ MissionMoon/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MissionMoon
+//
+//  Created by Mete Alp Kizilcay on 25.10.2020.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MissionMoon/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  MissionMoon
+//
+//  Created by Mete Alp Kizilcay on 25.10.2020.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+
+                    Text(self.astronaut.description)
+                        .padding()
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
